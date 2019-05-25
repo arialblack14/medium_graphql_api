80388bb617b86ed69980b95fbce5877f02725b5a
Alex Tsiras <tsirasalexis@yahoo.gr>
2019-05-25 11:32:29 +0300

    Add session resolver

---
+++ lib/medium_graphql_api/accounts/session.ex
@@ -0,0 +1,16 @@
+defmodule MediumGraphqlApi.Accounts.Session do
+  alias MediumGraphqlApi.Accounts.User
+  alias MediumGraphqlApi.Repo
+
+  def authenticate(args) do
+    user = Repo.get_by(User, email: String.downcase(args.email))
+
+    case check_password(user, args) do
+      true -> {:ok, user}
+      _ -> {:error, "Invalid login credentials"}
+    end
+  end
+
+  defp check_password(nil, args), do: Argon2.dummy_checkpw()
+  defp check_password(user, args), do: Argon2.checkpw(args.password, user.password_hash)
+end
---
+++ lib/medium_graphql_api_web/resolvers/session_resolver.ex
@@ -0,0 +1,5 @@
+defmodule MediumGraphqlApiWeb.Resolvers.SessionResolver do
+  def login_user(_, %{input: input}, _) do
+    {:ok, %{token: jwt_token, user: user}}
+  end
+end
